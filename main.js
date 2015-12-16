6b1a0d1812d96148ea14ce9929978a1a01cfb628
xnerijusx <nerijus.plytninkas@gmail.com>
2015-12-16 19:43:24 +0200

    Lesson2
    Prisijungimo forma su JavaScript. Taip, kaip norejau ir tiek, kiek siam
    momentui leidzia mano zinios :)
    Kas gerai, kas ne?

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-Function clicked(){
-	var x =document.getElementsByName('user');
-	var y = document.getElementsByName('pass');
-}
+function clicked(){
+  var x = document.getElementsByName('user')[0].value;
+  var y = document.getElementsByName('pass')[0].value;
+           if(x===""&&y==="")
+           return alert("Jūs neįvedėte prisijungimo\n vardo ir slaptažodžio");
+           else{  
+           if(x==="")
+           return alert("Jūs neįvedėte prisijungimo\n vardo");
+           else{
+           if(y==="")
+           return alert("Jūs neįvedėte prisijungimo\n slaptažodžio");
+           if(y.length<4)
+           return alert("Slaptažodis turi būti ne mažiau 4 simbolių ");
+  return alert("Jus prisijungete vardu:\n"+x)
+}}}
+
+
 
 
 
